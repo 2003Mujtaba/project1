01f192cc545b0d22ba7ffef0850cce30201bfd3d
Hajisuliman <sulimanqaderi@gmail.com>
2023-12-02 16:08:08 -0500

    Part 2 Version 2.0

--- routes/surveycontroller.js
+++ routes/surveycontroller.js
@@ -1,10 +1,10 @@
-const Survey = require('../models/survey'); // Import the Survey model
+const Survey = require("../models/survey"); // Import the Survey model
 
 // Display list of all surveys
 exports.listSurveys = async (req, res) => {
   try {
     const surveys = await Survey.find();
-    res.render('surveys/list-survey', { surveys });
+    res.render("surveys/list-survey", { surveys });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -14,7 +14,7 @@ exports.listSurveys = async (req, res) => {
 exports.surveyDetail = async (req, res) => {
   try {
     const survey = await Survey.findById(req.params.id);
-    res.render('surveys/survey-detail', { survey });
+    res.render("surveys/survey-detail", { survey });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -22,7 +22,7 @@ exports.surveyDetail = async (req, res) => {
 
 // Display survey create form on GET
 exports.surveyCreateGet = (req, res) => {
-  res.render('surveys/new-survey');
+  res.render("surveys/new-survey");
 };
 
 // Handle survey create on POST
@@ -31,10 +31,10 @@ exports.surveyCreatePost = async (req, res) => {
     const survey = new Survey({
       title: req.body.title,
       description: req.body.description,
-      questions: req.body.questions,               // Make sure to structure the questions input correctly on the client-side
+      questions: req.body.questions, // Make sure to structure the questions input correctly on the client-side
     });
     await survey.save();
-    res.redirect('/surveys');
+    res.redirect("/surveys");
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -44,18 +44,17 @@ exports.surveyCreatePost = async (req, res) => {
 exports.surveyDeleteGet = async (req, res) => {
   try {
     await Survey.findByIdAndDelete(req.params.id);
-    res.redirect('/surveys');
+    res.redirect("/surveys");
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-
 // Display survey update form on GET
 exports.surveyUpdateGet = async (req, res) => {
   try {
     const survey = await Survey.findById(req.params.id);
-    res.render('surveys/edit-survey', { survey });
+    res.render("surveys/edit-survey", { survey });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -65,7 +64,7 @@ exports.surveyUpdateGet = async (req, res) => {
 exports.surveyUpdatePost = async (req, res) => {
   try {
     const survey = await Survey.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect('/surveys');
+    res.redirect("/surveys");
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -78,21 +77,27 @@ exports.surveySubmitPost = async (req, res) => {
     const survey = await Survey.findById(req.params.id);
 
     // Create a new response object based on the submitted answers
-    const newResponse = {
-      responses: req.body.questions.map((question, index) => {
-        return {
-          questionText: survey.questions[index].questionText,
-          response: req.body[`question_${index}`]
-        };
-      })
-    };
+    console.log(req.body);
+    if (Array.isArray(req.body.questions)) {
+      const newResponse = {
+        responses: req.body.questions.map((question, index) => {
+          return {
+            questionText: survey.questions[index].questionText,
+            response: req.body[`question_${index}`],
+          };
+        }),
+      };
+      console.log(newResponse);
 
-    // Update the survey with the new response
-    survey.responses.push(newResponse);
-    await survey.save();
+      // Update the survey with the new response
+      survey.responses.push(newResponse);
+      await survey.save();
+    } else {
+      return res.redirect("/surveys");
+    }
 
     // Redirect or send a success message
-    res.redirect('/surveys/' + req.params.id);
+    res.redirect("/surveys/" + req.params.id);
   } catch (error) {
     res.status(500).send(error.message);
   }
